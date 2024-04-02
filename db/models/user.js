7c0f1846ebccd0ac4d21bcea6583e044eca6b086
ovti <71078808+ovti@users.noreply.github.com>
2024-04-02 16:19:56 +0200

    update the user model

--- db/models/user.js
+++ db/models/user.js
@@ -2,19 +2,25 @@ const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
 const sequelize = require('../db');
 
-const User = sequelize.define('user', {
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: { len: [3, undefined] },
+const User = sequelize.define(
+  'user',
+  {
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { len: [3, undefined] },
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: { len: [3, undefined] },
+    },
   },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: { len: [3, undefined] },
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 User.beforeCreate(async (user) => {
   const hashedPassword = await bcrypt.hash(user.password, 10);
