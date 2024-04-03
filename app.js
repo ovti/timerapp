71fe2c41afb95301810659bd57170bcf7685ef51
ovti <svdovti@gmail.com>
2024-04-04 00:48:37 +0200

    Update view engine to ejs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 require('./db/db');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
